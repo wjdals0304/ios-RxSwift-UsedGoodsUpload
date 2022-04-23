782daa3ad0db68d8e6e97d2d39f230ac86347ac2
jmkim <wjdals0304@gmail.com>
2022-04-23 20:20:47 +0900

    add: main model

---
+++ UsedGoodsUploadAPP/Presentation/MainViewController/MainModel.swift
@@ -0,0 +1,18 @@
+//
+//  MainModel.swift
+//  UsedGoodsUploadAPP
+//
+//  Created by 김정민 on 2022/04/23.
+//
+
+import Foundation
+
+struct MainModel {
+    
+    func setAlert(errorMessage: [String]) -> Alert {
+        let title = errorMessage.isEmpty ? "성공" : "실패"
+        let message = errorMessage.isEmpty ? nil : errorMessage.joined(separator: "\n")
+        return (title: title, message: message)
+        
+    }
+}
