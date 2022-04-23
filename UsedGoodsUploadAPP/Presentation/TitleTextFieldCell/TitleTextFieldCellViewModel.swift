ca72c0615e2dc8ea92afca2ec30cfe05bcb0527f
jmkim <wjdals0304@gmail.com>
2022-04-23 13:05:18 +0900

    add: titleTextField view Model add

---
+++ UsedGoodsUploadAPP/Presentation/TitleTextFieldCell/TitleTextFieldCellViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  TitleTextFieldCellViewModel.swift
+//  UsedGoodsUploadAPP
+//
+//  Created by 김정민 on 2022/04/23.
+//
+
+import RxCocoa
+
+struct TitleTextFieldCellViewModel {
+    let titleText = PublishRelay<String?>()
+}
